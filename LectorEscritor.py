368226429fc5ce10347babcc40ac218bcf4b7a62
d1ego <dipacmon@live.com>
2020-03-03 09:53:18 -0600

    Add files via upload
    Ejercicio declarando 2 lectores y 2 escritores
    
    Un Lector accede al archivo original de texto
    Después se hace un lock
    Se manda llamar a un escritor
    Este hace una modificación al archivo de texto, 
    Se hace un lock
    Se manda llamar al segundo lector, que a su vez repite el proceso para llamar al segundo escritor
    
    Limitado en este caso hasta 5 turnos

---
+++ LectorEscritor.py
@@ -0,0 +1,59 @@
+import time
+import threading
+from threading import Thread
+import inspect
+
+class Thread(threading.Thread):
+    def __init__(self, t, *args):
+        threading.Thread.__init__(self, target=t, args=args)
+        self.start()
+
+contador=0
+f = open("texto.txt", "r")
+a = open("texto.txt", "a")
+
+lock = threading.Lock()
+
+def conteo():
+    global contador
+    caller = inspect.getouterframes(inspect.currentframe())[1][3]
+    with lock:
+        print
+        "Lock Acquired"
+        contador += 1
+        time.sleep(2)
+
+def lector1():
+    while contador<5:
+        print("Hola soy el lector 1 y el archivo de texto dice:")
+        print(f.read())
+        conteo()
+        escritor1()
+
+def lector2():
+    while contador <5:
+        print("Hola soy el lector 2 y el archivo de texto dice:")
+        print(f.read())
+        conteo()
+        escritor2()
+
+def escritor1():
+    while contador <5:
+        print("Hola soy el escritor 1 y agregue texto al libro:")
+        a.write("\n+++++++Esta linea ha sido escrita por el escritor 1\n")
+        conteo()
+        lector2()
+
+def escritor2():
+    while contador <5:
+        print("Hola soy el escritor 2 y agregue texto al libro:")
+        a.write("\n-------Esta linea ha sido escrita por el escritor 2\n")
+        conteo()
+        lector1()
+
+
+def main():
+    Lector1=Thread(lector1())
+
+
+main()
